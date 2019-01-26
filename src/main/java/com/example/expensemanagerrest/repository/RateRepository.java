bd8289d4577429b2a6d9e0f40ca4c7cdfde9e6bd
ovidiu <enescu_vd@yahoo.com>
2019-01-26 18:03:44 +0200

    added rate repository

---
+++ src/main/java/com/example/expensemanagerrest/repository/RateRepository.java
@@ -0,0 +1,16 @@
+package com.example.expensemanagerrest.repository;
+
+import com.example.expensemanagerrest.model.Rate;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by Ovidiu on 26-Jan-19.
+ */
+@Repository
+public interface RateRepository extends JpaRepository<Rate, Long> {
+
+  void deleteByIdIn(List<Long> list);
+
+}
