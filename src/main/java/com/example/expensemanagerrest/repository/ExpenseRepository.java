23de10bd327b4b51f9d4ca8755547e0ab52c8152
ovidiu <enescu_vd@yahoo.com>
2019-01-26 18:03:26 +0200

    added expense repository

---
+++ src/main/java/com/example/expensemanagerrest/repository/ExpenseRepository.java
@@ -0,0 +1,15 @@
+package com.example.expensemanagerrest.repository;
+
+import com.example.expensemanagerrest.model.Expense;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by Ovidiu on 26-Jan-19.
+ */
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+
+  void deleteByIdIn(List<Long> list);
+}
