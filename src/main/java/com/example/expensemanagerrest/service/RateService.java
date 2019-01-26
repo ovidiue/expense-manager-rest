5acbfa93de11d5ab4a810c99bcbe1ee485385fd9
ovidiu <enescu_vd@yahoo.com>
2019-01-26 18:09:38 +0200

    added rate service

---
+++ src/main/java/com/example/expensemanagerrest/service/RateService.java
@@ -0,0 +1,40 @@
+package com.example.expensemanagerrest.service;
+
+import com.example.expensemanagerrest.model.Rate;
+import com.example.expensemanagerrest.repository.RateRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by Ovidiu on 26-Jan-19.
+ */
+@Service
+public class RateService {
+
+  @Autowired
+  private RateRepository rateRepository;
+
+  public List<Rate> findAll() {
+    return this.rateRepository.findAll();
+  }
+
+  public void saveRate(Rate rate) {
+    this.rateRepository.save(rate);
+  }
+
+  public void deleteRate(Rate rate) {
+    this.rateRepository.delete(rate);
+  }
+
+  public Rate getRate(Long catId) {
+    return this.rateRepository.getOne(catId);
+  }
+
+  @Transactional
+  public void deleteRates(List<Long> list) {
+    this.rateRepository.deleteByIdIn(list);
+  }
+
+}
