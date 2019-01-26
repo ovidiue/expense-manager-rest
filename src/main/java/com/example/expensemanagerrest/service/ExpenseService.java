089ae46207754df645ebfcc3443bb0eac61cc1a5
ovidiu <enescu_vd@yahoo.com>
2019-01-26 18:09:21 +0200

    added expense service

---
+++ src/main/java/com/example/expensemanagerrest/service/ExpenseService.java
@@ -0,0 +1,40 @@
+package com.example.expensemanagerrest.service;
+
+import com.example.expensemanagerrest.model.Expense;
+import com.example.expensemanagerrest.repository.ExpenseRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by Ovidiu on 26-Jan-19.
+ */
+@Service
+public class ExpenseService {
+
+  @Autowired
+  private ExpenseRepository expenseRepository;
+
+  public List<Expense> findAll() {
+    return this.expenseRepository.findAll();
+  }
+
+  public void saveExpense(Expense expense) {
+    this.expenseRepository.save(expense);
+  }
+
+  public void deleteExpense(Expense expense) {
+    this.expenseRepository.delete(expense);
+  }
+
+  public Expense getExpense(Long catId) {
+    return this.expenseRepository.getOne(catId);
+  }
+
+  @Transactional
+  public void deleteExpenses(List<Long> list) {
+    this.expenseRepository.deleteByIdIn(list);
+  }
+
+}
